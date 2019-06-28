fd5c248517e9dbbf7a3000cadf205fa2f86e7621
pmartin95 <martip8@rpi.edu>
2019-06-28 09:07:18 -0700

    Added more elementary array topics

--- ex10.f90
+++ ex10.f90
@@ -2,16 +2,32 @@
 program ex10
   implicit none
   !Two ways of declaring an array
-  real, dimension(2,3) :: x
-  real ::  y(6)
+  integer,dimension(6) :: a
+  real, dimension(0:9) :: b
+  logical, dimension(2,2) :: yes_no
+
+  integer, dimension(8) :: x, z(16)
+  real :: alpha(1:3), beta(4:9)
+  real, dimension(0:5,12:45,6) :: data
+  character(len=10) :: names(25)
+
+  real ::  y(9)
+  integer :: i
+
   !
   ! y = 1.0
   ! x = 5*y + 1
   ! write(*,*) x,y
+  !Fortran indexes starting from 1. We can assign values easily with loops
 
+  do i =1,size(y)
+    y(i) = (i-1)/real(size(y))
+  end do
 
-  y = (/1,2,3,4,5,6/)
+  !We can print an array element by element or all at the same time
+  do i =1,6
+    write(*,*) y(i)
+  end do
   write(*,*) y
-  x= reshape(y, (/2,3 /))
-  write(*,*) x(1,:)
+
 end program
