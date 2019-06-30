03327d067664fd5afb9729059a9ec2940ac82718
pmartin95 <martip8@rpi.edu>
2019-06-30 13:53:47 -0700

    I briefly do an example on select cases

--- ex8.f90
+++ ex8.f90
@@ -1,17 +1,21 @@
+!While not particularly useful to us, here is how to use Fortran's version
+!of select cases
+
 program ex8
   implicit none
   integer :: month
-   month = 1
+  month = 1
+
   select case (month)
-  case(4,5)
-    write(*,*) "Spring"
-  case(6,7)
-    write(*,*) "Summer"
-  case(8:10)
-    write(*,*) "Autumn"
-  case(1:3,11:12)
-    write(*,*) "Winter"
-  case default
-    write(*,*) " Not a month"
+    case(4,5)
+      write(*,*) "Spring"
+    case(6,7)
+      write(*,*) "Summer"
+    case(8:10)
+      write(*,*) "Autumn"
+    case(1:3,11:12)
+      write(*,*) "Winter"
+    case default
+      write(*,*) " Not a month"
   end select
 end program
